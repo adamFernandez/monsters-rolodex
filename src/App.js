296bdafd1bec744012c21a7b5985a5b670b6091e
Adam Fernandez <aarodriguezfernandez@gmail.com>
2020-02-11 12:35:44 +0000

    filtering the state of the search bar

--- src/App.js
+++ src/App.js
@@ -19,6 +19,11 @@ class App extends Component {
     .then(users => this.setState({ monsters: users}))
   }
   render(){
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+      )
+
     return (
       <div className="App">
         <input 
@@ -27,7 +32,7 @@ class App extends Component {
           onChange={ e => 
             this.setState({ serchField: e.target.value })}
         />
-      <CardList monsters={this.state.monsters} />
+      <CardList monsters={filteredMonsters} />
         
       </div>
     );
